07df59a4124e77abb584e7ae3f1cd0b22b96609f
121132 <1271194370@qq.com>
2024-02-28 16:17:18 +0800

    test

--- Appreciation_of_Wilderness/app01/views.py
+++ Appreciation_of_Wilderness/app01/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def init(request):
+    if request.method == "POST":
+        name = request.POST.get("x")
+        print(name)
+    return render(request, 'init.html')
