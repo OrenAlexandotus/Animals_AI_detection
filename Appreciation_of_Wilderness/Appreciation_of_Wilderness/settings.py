5f73a4d8a6714ec2a97045a72733446ca4fd3a20
121132 <1271194370@qq.com>
2024-03-01 10:40:56 +0800

    连接数据库要修改setting文件

--- Appreciation_of_Wilderness/Appreciation_of_Wilderness/settings.py
+++ Appreciation_of_Wilderness/Appreciation_of_Wilderness/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'duiduidui',
         'USER': 'root',
-        'PASSWORD': 'lx200261',
+        'PASSWORD': '',#修改自己的密码
         'HOST': '127.0.0.1',
-        'PORT': '3307'
+        'PORT': ''#和端口
     }
 }
 
